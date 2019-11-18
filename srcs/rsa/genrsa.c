b90fc9235259f8dbaf1408b30e0cc260958ead45
rpeepz <r.papagna.42@gmail.com>
2019-11-17 22:03:06 -0800

    still needs work

--- srcs/rsa/genrsa.c
+++ srcs/rsa/genrsa.c
@@ -6,7 +6,7 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 22:56:10 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/11/16 16:51:56 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/11/17 21:06:55 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,15 +45,15 @@ __uint64_t		genprime(int bits)
 	__uint64_t			max;
 	unsigned int		i;
 
-	min = 1L << (abs(bits - 1));
-	max = (1L << bits) - 1;
+	min = 1ULL << (abs(bits - 1));
+	max = (1ULL << bits) - 1;
 	i = 0;
 	while (1)
 	{
 		if (i % (bits * 2) == 0)
 		{
 			p = genrand(min, max) | 1;
-			p |= 1UL << (bits - 1);
+			p |= 1ULL << (bits - 1);
 			ft_putchar_fd('.', 2);
 		}
 		if (ft_is_primary(p, 9.0F))
@@ -73,10 +73,19 @@ __uint64_t		valid_modulus(t_rsa gg, int bits)
 
 	(void)bits;
 	n = gg.p * gg.q;
-//	if (n & 1ULL << (bits - 1))
+	if (n & 1ULL << (bits - 1))
 		return (n);
-//	return (0);
+	return (n);
 }
+
+/*
+**	Test primes with errors
+**	gg.p = 61871;
+**	gg.q = 47279;
+**
+**	gg.p = 3620177833;
+**	gg.q = 2455139927;
+*/
 
 __uint64_t		genrsa(t_rsa_out rsa)
 {
@@ -87,13 +96,13 @@ __uint64_t		genrsa(t_rsa_out rsa)
 	ft_sprintf(buf, "Generating RSA private key, %d bit long modulus\n",
 	rsa.bits);
 	ft_putstr_fd(buf, 2);
-//	while (1)
-//	{
+	while (1)
+	{
 		gg.p = genprime(rsa.bits / 2);
 		gg.q = genprime(rsa.bits / 2);
-		gg.n = valid_modulus(gg, rsa.bits);
-//			break ;
-//	}
+		if ((gg.n = valid_modulus(gg, rsa.bits)))
+			break ;
+	}
 	gg.e = 65537;
 	gg.phi = (gg.p - 1) * (gg.q - 1);
 	gg.d = mod_inverse(gg.e, gg.phi);
