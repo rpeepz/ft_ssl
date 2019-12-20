8e70277da09ec481c71d12c41fa87c5b24e5e572
rpeepz <r.papagna.42@gmail.com>
2019-12-20 01:23:46 -0800

    parsing for des cipher

--- srcs/cipher/des_args.c
+++ srcs/cipher/des_args.c
@@ -6,7 +6,7 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/18 20:52:54 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/12/19 01:40:21 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/12/20 01:23:25 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,54 +15,54 @@
 
 static int			verify_arg(t_parse *des, char *fname, int type, int i)
 {
-	if (type & 1 || type & 2)
+	if ((!fname || !*fname))
 	{
-		if ((!fname || !*fname))
-			dprintf(2, "missing %s argument for -%c\n",\
-			type & 1 ? "IV" : "key", type & 1 ? 'v' : 'k');
-		else
-		{
-			while (fname[++i])
-				if (!ft_isdigit(fname[i]) &&
-				!(ft_toupper(fname[i]) >= 'A' && ft_toupper(fname[i]) <= 'F'))
-					return (type & 1 ? -2 : -3);
-		}
-		des->args->iv = (type & 1) ? ft_hextoull(fname) : des->args->iv;
-		des->args->key = (type & 2) ? ft_hextoull(fname) : des->args->key;
+		type & D_VECT ? dprintf(2, "missing IV argument for -v\n") : 0;
+		type & D_KEY ? dprintf(2, "missing key argument for -k\n") : 0;
+		type & D_SALT ? dprintf(2, "missing salt argument for -s\n") : 0;
+		return (-1);
 	}
-	else if (type & 3)
+	else
 	{
-		if (!fname || !*fname)
-			ft_putstr_fd("missing argument for '-s'\n", 2);
-		else
-			des->args->salt = (uint8_t *)fname;
-		des->args->salt_len = ft_strlen((char *)des->args->salt);
+		while (fname[++i])
+			if (!ft_isdigit(fname[i]) &&
+			!(ft_toupper(fname[i]) >= 'A' && ft_toupper(fname[i]) <= 'F'))
+			{
+				ft_putstr_fd("non-hex digit\ninvalid hex ", 2);
+				type & D_VECT ? ft_putstr_fd("iv", 2) : 0;
+				type & D_KEY ? ft_putstr_fd("key", 2) : 0;
+				type & D_SALT ? ft_putstr_fd("salt", 2) : 0;
+				ft_putstr_fd(" value\n", 2);
+				return (-type);
+			}
 	}
-	return (!fname ? -1 : 0);
+	des->args->iv = (type & D_VECT) ? ft_hextoull(fname) : des->args->iv;
+	des->args->key = (type & D_KEY) ? ft_hextoull(fname) : des->args->key;
+	des->args->salt = (type & D_SALT) ? ft_hextoull(fname) : des->args->salt;
+	return (0);
 }
 
-/*
-**	may need to malloc on line 38
-*/
-
-static int			check_input(t_parse *des, char *arg, char *fname, int tmp)
+static int			check_input(t_parse *des, char *arg, char *fname)
 {
-	if ((!ft_strcmp(arg, "v") || !ft_strcmp(arg, "iv")) &&\
+	if ((!ft_strcmp(arg, "v") || !ft_strcmp(arg, "-iv")) &&\
 	(des->flag |= D_VECT))
-		return (verify_arg(des, fname, 1, -1));
+		return (verify_arg(des, fname, D_VECT, -1));
 	else if ((!ft_strcmp(arg, "k") || !ft_strcmp(arg, "-key")) &&\
 	(des->flag |= D_KEY))
-		return (verify_arg(des, fname, 2, -1));
+		return (verify_arg(des, fname, D_KEY, -1));
 	else if ((!ft_strcmp(arg, "s") || !ft_strcmp(arg, "-salt")) &&\
 	(des->flag |= D_SALT))
-		return (verify_arg(des, fname, 3, -1));
+		return (verify_arg(des, fname, D_SALT, -1));
 	else if ((!ft_strcmp(arg, "p") || !ft_strcmp(arg, "-pass")) &&\
 	(des->flag |= D_PASS))
 	{
-		!fname ? dprintf(2, "missing source argument for '-p'\n") : 0;
-		if (open_file_to_fd(&tmp, fname, 0))
+		if (!fname || !*fname)
+		{
+			ft_putstr_fd("missing source argument for '-p'\n", 2);
 			return (-1);
-		close(tmp);
+		}
+		else
+			des->args->pass = ft_strdup(fname);
 	}
 	else
 		return (1);
@@ -92,7 +92,7 @@ static int			valid_arg(t_parse *des, char *arg, char *fname, int tmp)
 	else if (!ft_strcmp(arg, "d") || !ft_strcmp(arg, "-decrypt"))
 		des->flag |= D_FLAG;
 	else
-		return (check_input(des, arg, fname, tmp));
+		return (check_input(des, arg, fname));
 	return (0);
 }
 
@@ -129,17 +129,14 @@ int					parse_des(char **av, t_parse *des, t_ssl *ssl, int i)
 			if ((des->mode = valid_arg(des, &av[i][1], av[i + 1], 0)))
 			{
 				ssl->flag = des->mode < 0 ? -42 : ssl->flag;
-				if (des->mode < -1)
-					dprintf(2, "non-hex digit\ninvalid hex %s value\n",
-					des->mode == -2 ? "iv" : "key");
+				if (des->mode < 0)
+					ssl->flag = -42;
 				return (ft_error(6, &av[i][1], ssl));
 			}
 			else if (des->fd_out == 1 && des->flag & F_OUT)
 				open_file_to_fd((int*)&des->fd_out, av[++i], 1);
 			else if ((!des->fd_in) && (des->flag & F_IN))
 				open_file_to_fd((int*)&des->fd_in, av[++i], 0);
-			else if ((!des->args->pass) && (des->flag & D_PASS))
-				open_file_to_fd((int*)&des->args->pass, av[++i], 0);
 			else
 				clear_arg(&i, des);
 		}
