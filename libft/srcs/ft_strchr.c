5bf9fa52562a11c079b42696a1f385cf123c4720
rpeepz <r.papagna.42@gmail.com>
2020-01-06 17:38:11 -0800

    return false added

--- libft/srcs/ft_strchr.c
+++ libft/srcs/ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpapagna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 18:05:05 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/02/19 22:16:41 by rpapagna         ###   ########.fr       */
+/*   Updated: 2020/01/06 17:24:09 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,16 +41,14 @@ char	*ft_strrchr(const char *s, int c)
 int		ft_strchri(const char *s, int c)
 {
 	size_t		i;
-	int			z;
 
 	i = 0;
-	z = 0;
 	if (!s)
-		return (0);
+		return (-1);
 	while (s[i])
 		if (s[i++] == (char)c)
 			return (--i);
-	return (z);
+	return (-1);
 }
 
 int		ft_strrchri(const char *s, int c)
